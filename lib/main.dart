aeff836559d121dba4fd82af1810c5d227463e87
tbgbarros <111811766+tbgbarros@users.noreply.github.com>
2024-08-13 22:23:47 -0300

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        home: Material(
+          color: const Color(0xFFB3B3B3),
+          child: Center(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Card(
+                  margin: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Image.asset('assets/img.png'),
+                          title: const Text('Imagem'),
+                          subtitle: const Text('O dia tem duas horas a mais pra quem é esforçado!'),
+                        ),
+                ],
+              ),
+            ),
+             Card(
+                margin: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Image.asset('assets/img.png'),
+                      title: const Text('Imagem'),
+                      subtitle: const Text('O dia tem duas horas a mais pra quem é esforçado!'),
+                    )
+                  ],
+                ),
+            ),
+          ],
+        ),
+      ),
+        ),
+    ),
+
+
+  );
+}
